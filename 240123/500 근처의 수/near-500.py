ce9e9401b18b4f6b79997a9423dd9fdc8fab6e75
Yudeenim <157336113+Yudeenim@users.noreply.github.com>
2024-01-23 21:15:21 +0900

    Title: 500 근처의 수, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240123/500 근처의 수/near-500.py
@@ -0,0 +1,13 @@
+arr = list(map(int, input().split()))
+
+max_val = 1
+min_val = 1000
+
+for elem in arr:
+	if elem < 500 and elem > max_val:
+		max_val = elem
+		
+	if elem > 500 and elem < min_val:
+		min_val = elem
+
+print(max_val, min_val)

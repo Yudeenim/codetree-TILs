7a31afcf39f00b222a0e4fade48abb98ea9517d6
Yudeenim <157336113+Yudeenim@users.noreply.github.com>
2024-01-28 05:53:36 +0900

    Title: 그 요일은, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240128/그 요일은/the-day-of-the-day.py
+++ 240128/그 요일은/the-day-of-the-day.py
@@ -1,17 +1,27 @@
-m1, d1, m2, d2 = map(int, input().split())
-A = input()
+m1,d1,m2,d2=tuple(map(int,input().split()))
+sumval=0
+cnt=0
+day=input()
+def mcal(m):
+    if (m>=8 and m%2==0) or (m<8 and m%2 !=0):
+        return 31
+    elif (m==2):
+        return 29
+    else:
+        return 30
 
-day_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-days_of_month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-start_day_dif = day_of_week.index(A)
+if (m1<m2) or (m1==m2 and (d2>=d1)):
+    for i in range(m1,m2):
+        sumval += mcal(i)
+    total = sumval+d2-d1
+    arr=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+    arrr=[0,0,0,0,0,0,0]
+    for i in range(total+1):
+        arrr[i%7] +=1
+    for i in range(7):
+        if arr[i]==day:
+            cnt = i
+    print(arrr[cnt])
 
-def days(m, d):
-    day = d
-    for i in range(1, m):
-        day += days_of_month[i]
-    return day
-
-if days(m2, d2) - days(m1, d1) < 7: 
-    print(0)
-else: 
-    print((days(m2, d2) - days(m1, d1) + start_day_dif) // 7)
+else:
+    print("0")

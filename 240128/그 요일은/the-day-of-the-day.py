55cb16abfd49aaf348d29bf20b6eb6ced7e9797b
Yudeenim <157336113+Yudeenim@users.noreply.github.com>
2024-01-28 02:54:28 +0900

    Title: 그 요일은, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240128/그 요일은/the-day-of-the-day.py
+++ 240128/그 요일은/the-day-of-the-day.py
@@ -1,14 +1,13 @@
-#m1월 d1일이 월요일, + 7일마다 월요일. 다음 달로 넘어가는 경우, +7 후 남는 day를 다음 달로 옮기기(- 달의 마지막 날.)
-
 m1, d1, m2, d2 = map(int, input().split())
 A = input()
 
 days = 0
-day_of_week = ["0", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+day_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 day_of_month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-for i in range(8):
-    if day_of_week[i] == A:
-        d1 += (i - 1)
+
+# 시작 날짜의 요일을 계산
+start_day_index = day_of_week.index(A)
+d1 += start_day_index
 
 while True:
     if m1 == m2 and d1 >= d2:
@@ -16,11 +15,11 @@ while True:
             days += 1
         break
 
-    days += 1                       #요일이 몇 번인지
-    d1 += 7                         #d1에서 7일을 늘려 요일 계산
+    days += 1
+    d1 += 7
 
     if d1 > day_of_month[m1]:
         m1 += 1
         d1 = d1 - day_of_month[m1]
-       
+
 print(days)

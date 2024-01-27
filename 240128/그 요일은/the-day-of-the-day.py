d5e63ab1bf5e8b641d43caca43cf3a424cb24e54
Yudeenim <157336113+Yudeenim@users.noreply.github.com>
2024-01-28 05:54:05 +0900

    Title: 그 요일은, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240128/그 요일은/the-day-of-the-day.py
+++ 240128/그 요일은/the-day-of-the-day.py
@@ -1,27 +1,51 @@
-m1,d1,m2,d2=tuple(map(int,input().split()))
-sumval=0
-cnt=0
-day=input()
-def mcal(m):
-    if (m>=8 and m%2==0) or (m<8 and m%2 !=0):
-        return 31
-    elif (m==2):
-        return 29
-    else:
-        return 30
+# 변수 선언 및 입력
+m1, d1, m2, d2 = tuple(map(int, input().split()))
+A = input()
 
-if (m1<m2) or (m1==m2 and (d2>=d1)):
-    for i in range(m1,m2):
-        sumval += mcal(i)
-    total = sumval+d2-d1
-    arr=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
-    arrr=[0,0,0,0,0,0,0]
-    for i in range(total+1):
-        arrr[i%7] +=1
-    for i in range(7):
-        if arr[i]==day:
-            cnt = i
-    print(arrr[cnt])
 
-else:
-    print("0")
+def num_of_days(m, d):
+    # 계산 편의를 위해 월마다 몇 일이 있는지를 적어줍니다. 
+    days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    total_days = 0
+    
+    # 1월부터 (m - 1)월 까지는 전부 꽉 채워져 있습니다.
+    for i in range(1, m):
+        total_days += days[i]
+    
+    # m월의 경우에는 정확이 d일만 있습니다.
+    total_days += d
+    
+    return total_days
+
+
+def num_of_day(s):
+    # 간단한 비교를 위해 요일을 숫자로 나타내줍니다.
+    if s == "Mon": 
+        return 0
+    elif s == "Tue": 
+        return 1
+    elif s == "Wed": 
+        return 2
+    elif s == "Thu": 
+        return 3
+    elif s == "Fri": 
+        return 4
+    elif s == "Sat": 
+        return 5
+    return 6
+
+
+ans = 0   
+start_date = num_of_days(m1, d1)
+end_date = num_of_days(m2, d2)
+cur_day = num_of_day("Mon")
+
+for date in range(start_date, end_date + 1):
+    # 오늘의 요일이 A요일과 같다면 정답에 추가합니다.
+    if cur_day == num_of_day(A): 
+        ans += 1
+
+    cur_day = (cur_day + 1) % 7
+
+# 출력
+print(ans)

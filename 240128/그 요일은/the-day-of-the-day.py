82e753eaea153e14ee0efd2bd367ead547aef55a
Yudeenim <157336113+Yudeenim@users.noreply.github.com>
2024-01-28 05:36:05 +0900

    Title: 그 요일은, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240128/그 요일은/the-day-of-the-day.py
+++ 240128/그 요일은/the-day-of-the-day.py
@@ -1,25 +1,17 @@
 m1, d1, m2, d2 = map(int, input().split())
 A = input()
 
-days = 0
 day_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-day_of_month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+days_of_month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+start_day_dif = day_of_week.index(A)
 
-# 시작 날짜의 요일을 계산
-start_day_index = day_of_week.index(A)
-d1 += start_day_index
+def days(m, d):
+    day = d
+    for i in range(1, m):
+        day += days_of_month[i]
+    return day
 
-while True:
-    if m1 == m2 and d1 >= d2:
-        if d1 == d2:
-            days += 1
-        break
-
-    days += 1
-    d1 += 7
-
-    if d1 > day_of_month[m1]:
-        m1 += 1
-        d1 = d1 - day_of_month[m1]
-
-print(days)
+if days(m2, d2) - days(m1, d1) < 7: 
+    print(0)
+else: 
+    print((days(m2, d2) - days(m1, d1) + start_day_dif) // 7)

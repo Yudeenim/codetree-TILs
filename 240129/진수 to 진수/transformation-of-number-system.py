593c53835a3c639da100a3824a8a30b593f078e1
Yudeenim <157336113+Yudeenim@users.noreply.github.com>
2024-01-29 04:59:24 +0900

    Title: 진수 to 진수, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240129/진수 to 진수/transformation-of-number-system.py
@@ -0,0 +1,17 @@
+a, b = tuple(map(int, input().split()))
+n = input()
+n = [int(digit) for digit in str(n)]
+digits = []
+num = 0
+for i in range(len(n)):
+    num = num * a + n[i]                #num = 10진수로 표현한 n
+
+while True:
+    if num < b:
+        digits.append(num)
+        break
+
+    digits.append(num % b)
+    num //= b
+
+print(int(''.join(map(str, digits))))

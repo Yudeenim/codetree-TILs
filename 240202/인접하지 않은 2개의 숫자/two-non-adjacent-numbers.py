2515ce466e4fe3fbe973ea81c10f18f5f217bf4b
Yudeenim <157336113+Yudeenim@users.noreply.github.com>
2024-02-02 21:55:41 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240202/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
@@ -0,0 +1,14 @@
+import sys
+
+INT_MIN = -sys.maxsize
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+ans = INT_MIN
+for i in range(n):
+    for j in range(i + 2, n):
+        if ans < arr[i] + arr[j]:
+            ans = arr[i] + arr[j]
+
+print(ans)

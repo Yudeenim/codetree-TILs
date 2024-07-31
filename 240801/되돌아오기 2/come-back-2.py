f49b44f0333811325312ca66292c52a331db7f62
Yudeenim <157336113+Yudeenim@users.noreply.github.com>
2024-08-01 07:34:55 +0900

    Title: 되돌아오기 2, Time: 124ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240801/되돌아오기 2/come-back-2.py
@@ -0,0 +1,27 @@
+dirs = input()
+x, y = 0, 0
+curr_dir = 3
+
+dxs = [1,  0, -1, 0];
+dys = [0, -1,  0, 1];
+
+flag = False;
+
+leng = len(dirs)
+
+for i in range(leng):
+	c_dir = dirs[i]
+	if c_dir == 'L':
+		curr_dir = (curr_dir - 1 + 4) % 4
+	elif c_dir == 'R':
+		curr_dir = (curr_dir + 1) % 4
+	else:
+		x, y = x + dxs[curr_dir], y + dys[curr_dir]
+
+	if x == 0 and y == 0:
+		print(i + 1)
+		flag = True
+		break
+
+if flag == False:
+	print(-1)

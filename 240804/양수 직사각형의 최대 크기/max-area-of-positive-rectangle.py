dccd81a70183e86601154f977b594586cd5065f5
Yudeenim <157336113+Yudeenim@users.noreply.github.com>
2024-08-04 05:41:19 +0900

    Title: 양수 직사각형의 최대 크기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240804/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240804/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -6,23 +6,24 @@ grid = [
 result = 0
 
 def in_grid(x, y, i, j):
-    return x >= 0 and y >= 0 and x + j <= m and y + i <= n
+    return x >= 0 and y >= 0 and x + j < m and y + i < n
 
 def is_pos(x, y, i, j):
-    for k in range(i):
-        for l in range(j):
-            if grid[y + k][x + l] < 0:
+    for k in range(j + 1):
+        for l in range(i + 1):
+            if grid[y + l][x + k] < 0:
                 return False
     return True
 
-def simulation():
+def simulation(x, y):
     global result
-    for x in range(m):
-        for y in range(n):
-            for i in range(1, n - y + 1):
-                for j in range(1, m - x + 1):
-                    if in_grid(x, y, i, j) and is_pos(x, y, i, j):
-                        result = max(result, i * j)
+    for k in range(n - y):
+        for l in range(m - x):
+            if in_grid(x, y, k, l) and is_pos(x, y, k, l):
+                result = max(result, (k + 1) * (l + 1))
 
-simulation()
+for a in range(n):
+    for b in range(m):
+        simulation(b, a)
+
 print(result)

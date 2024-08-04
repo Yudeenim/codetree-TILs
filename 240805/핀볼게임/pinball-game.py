1011424d146f669d7b99b14c02d81d4c30db99a7
Yudeenim <157336113+Yudeenim@users.noreply.github.com>
2024-08-05 08:26:04 +0900

    Title: 핀볼게임, Time: 5086ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

--- 240805/핀볼게임/pinball-game.py
+++ 240805/핀볼게임/pinball-game.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
@@ -12,22 +14,26 @@ def get_max_time(n, grid):
         return current_dir
     
     def simulate(start_x, start_y, direction):
-        x, y, dir = start_x, start_y, direction
-        time = 0
-        while 0 <= x < n and 0 <= y < n:
-            time += 1
+        q = deque([(start_x, start_y, direction, 1)])
+        max_time = 0
+        while q:
+            x, y, dir, time = q.popleft()
             dx, dy = directions[dir]
             x, y = x + dx, y + dy
             if 0 <= x < n and 0 <= y < n:
                 dir = next_direction(dir, grid[x][y])
-        return time + 1
+                q.append((x, y, dir, time + 1))
+            else:
+                max_time = max(max_time, time)
+        return max_time
 
     max_time = 0
     for i in range(n):
         max_time = max(max_time, simulate(0, i, 1))  
-        max_time = max(max_time, simulate(n - 1, i, 3)) 
-        max_time = max(max_time, simulate(i, 0, 0))  
+        max_time = max(max_time, simulate(n - 1, i, 3))
+        max_time = max(max_time, simulate(i, 0, 0)) 
         max_time = max(max_time, simulate(i, n - 1, 2)) 
+    
     return max_time
 
-print(get_max_time(n, grid))
+print(get_max_time(n, grid)+1)
